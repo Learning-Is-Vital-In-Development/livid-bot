aa5616086f29b0f28a5bb278baa577fc28e9cb6f
haril song <songkg7@gmail.com>
2024-07-08 22:23:47 +0900

    refactor: change options order

--- bot/bot.go
+++ bot/bot.go
@@ -25,15 +25,15 @@ var (
 			Description: "Submit a link",
 			Options: []*discordgo.ApplicationCommandOption{
 				{
-					Type:        discordgo.ApplicationCommandOptionString,
-					Name:        "link",
-					Description: "Link to submit",
-					Required:    true,
-				},
-				{
 					Type:        discordgo.ApplicationCommandOptionAttachment,
 					Name:        "screenshot",
 					Description: "Screenshot of problem solution",
+					Required:    true,
+				},
+				{
+					Type:        discordgo.ApplicationCommandOptionString,
+					Name:        "link",
+					Description: "Link to submit",
 					Required:    true,
 				},
 			},
